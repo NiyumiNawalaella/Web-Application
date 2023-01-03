6c1f2fb336e94e749b8a2e8b1e8cba64873d429a
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-01-04 04:19:01 +0530

    changes made

--- format/format.js
+++ format/format.js
@@ -1,24 +1,4 @@
-$(window).on("scroll", function() {
-    var size = 400 - Math.min(400, $(document).scrollTop());
-    $("#logo img").width(size).height(size).margin(center);
-    // var requiredOffset = 0;
-    // var percentage = Math.max(1,$(document).scrollTop());
-    // var marginleft = requiredOffset * (1-percentage);
-
-    // $("#logo img").css('margin-left',marginleft);
-
-});
-// margin-top: 80px;
-//     margin-left: 510px;
-//     690px
-// $(document).ready(funtion()
-// {
-//     $(window).on("scroll",(funtion()))
-//     // {
-//     //     var scrollpos = $(window).scrollTop();
-//     //     $("#logo").css(
-//     //         {
-//     //             'height': +(scrollpos)+ 'px'
-//     //         })
-//     // })
-// })
+//$(window).on("scroll", function() {
+  //  var size = 400 - Math.min(400, $(document).scrollTop());
+    //$("#logo img").width(size).height(size).margin(center);
+//});
