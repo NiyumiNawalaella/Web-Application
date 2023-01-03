2c047460413a2078ab9ea2a1c778b45c1dee1f03
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-01-04 01:58:00 +0530

    Changes made to the format of web application

--- format/format.js
+++ format/format.js
@@ -1,7 +1,6 @@
 $(window).on("scroll", function() {
     var size = 400 - Math.min(400, $(document).scrollTop());
-    $("#logo img").width(size).height(size);
-
+    $("#logo img").width(size).height(size).margin(center);
     // var requiredOffset = 0;
     // var percentage = Math.max(1,$(document).scrollTop());
     // var marginleft = requiredOffset * (1-percentage);
