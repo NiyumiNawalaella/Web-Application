236f36a32979090707fc493433adb1f09675c0e5
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-01-16 14:14:34 +0530

    changes made

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare var name : any
+// declare var swiper :any
+// new name();
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    new name();
+    // new swiper();
+  }
 }
+
