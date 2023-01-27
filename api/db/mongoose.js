101972b21f83d321c4271b9e82556f91c995453b
NiyumiNawalaella <87523999+NiyumiNawalaella@users.noreply.github.com>
2023-01-27 20:49:05 +0530

    connected to mongodb

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,19 +2,20 @@
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Sportsclubapp', {useNewUrlParser: true}).then(() => {
+mongoose.set("strictQuery", false);
+mongoose.connect('mongodb://127.0.0.1:27017/Sportsclubapp', {useNewUrlParser: true}).then(() => {
     console.log("Connected to MongoDb sucessfully!");
 
 }).catch((e) => {
     console.log("Error while attempting to connect to MongoDB");
     console.log(e);
 });
-
 // to prevent deprectation warnings (from MongoDb native driver)
 // mongoose.set('useCreateIndex', true);
 // mongoose.set('useFindAndModify', false);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useUnifiedTopology', true);
+
 
 module.exports = {
     mongoose
